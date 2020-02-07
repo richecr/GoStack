12df79fe4fedee5d33e354c9a930cdc5ccfedce2
rickecr <richelton14@gmail.com>
2020-02-07 16:28:48 -0300

    CRUD

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,14 +2,49 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query Params = ?teste=1
 // Route Params = /users/1
 // Request Body = { "name": "Rick", "email": "rich@elton.com" }
 
-server.get('/users/:id', (req, res) => {
-  const { id } = req.params;
+// CRUD - Create, Read, Update, Delete
 
-  return res.json({ message: `Buscando por user com ID: ${id}` });
+const users = ['Rick', 'Lucas', 'Davi'];
+
+server.get('/users', (req, res) => {
+  return res.json(users)
+})
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json( users[index] );
+})
+
+server.post('/users', (req, res) => {
+  const { nome } = req.body;
+
+  users.push(nome);
+
+  return res.json(users);  
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { nome } = req.body;
+
+  users[index] = nome;
+
+  return res.json(users);
+})
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 })
 
 server.listen(3000);
