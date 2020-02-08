42738e9526cdf1e93423adc875dd47077ab327f6
rickecr <richelton14@gmail.com>
2020-02-07 22:25:40 -0300

    Criando loader de models

--- 01 - NodeJS/gobaber/src/routes.js
+++ 01 - NodeJS/gobaber/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello, Rick!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rick',
+    email: 'rick@elton.com',
+    password_hash: '123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
