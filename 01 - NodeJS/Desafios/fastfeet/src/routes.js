615639c88d075a0fc9c87cedf3549f5493cfab28
rickecr <richelton14@gmail.com>
2020-02-09 11:50:07 -0300

    Eslint, prettier e editorconfig configurado

--- 01 - NodeJS/Desafios/fastfeet/src/routes.js
+++ 01 - NodeJS/Desafios/fastfeet/src/routes.js
@@ -1,9 +1,7 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: "Oi" });
-})
+routes.get('/', (req, res) => res.json({ message: 'Oi' }));
 
-module.exports = routes;
+export default routes;
