f44d24c956468dbc1a1b08ca005e4aa507338804
rickecr <richelton14@gmail.com>
2020-02-19 17:33:31 -0300

    Iniciando com Redux: Muito massa <3

--- 03 - Flux/rocketshoes/src/pages/Home/index.js
+++ 03 - Flux/rocketshoes/src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 
@@ -6,7 +7,7 @@ import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends React.Component {
+class Home extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -25,6 +26,15 @@ export default class Home extends React.Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -35,7 +45,10 @@ export default class Home extends React.Component {
             <img src={product.image} alt={product.title} />
             <strong>{product.title} </strong>
             <span>{product.priceFormatted}</span>
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />
               </div>
@@ -47,3 +60,5 @@ export default class Home extends React.Component {
     );
   }
 }
+
+export default connect()(Home);
