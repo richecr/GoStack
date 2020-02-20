c8b3a4e250552fb7a40726dda79b563f19a68df9
rickecr <richelton14@gmail.com>
2020-02-20 17:34:08 -0300

    Configurando redux-saga

--- 03 - Flux/rocketshoes/src/pages/Home/index.js
+++ 03 - Flux/rocketshoes/src/pages/Home/index.js
@@ -29,10 +29,10 @@ class Home extends React.Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -48,7 +48,7 @@ class Home extends React.Component {
             <span>{product.priceFormatted}</span>
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />{' '}
