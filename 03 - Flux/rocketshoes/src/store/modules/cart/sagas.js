b9f7ddde4bb04ecb1d889b50734819ae8b2545c4
rickecr <richelton14@gmail.com>
2020-02-21 15:59:46 -0300

    Reactotron com redux-saga configurado e refatoramento nas actions

--- 03 - Flux/rocketshoes/src/store/modules/cart/sagas.js
+++ 03 - Flux/rocketshoes/src/store/modules/cart/sagas.js
@@ -1,13 +1,30 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
 
-import { addToCartSuccess } from './actions';
+import { formatPrice } from '../../../util/format';
+import { addToCartSuccess, updateAmount } from './actions';
 
 // * => Como se tivesse usando o async/await.
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state => {
+    return state.cart.find(p => p.id === id);
+  });
 
-  yield put(addToCartSuccess(response.data));
+  if (productExists) {
+    const amount = productExists.amount + 1;
+
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
